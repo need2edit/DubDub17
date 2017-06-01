b257febdfed1b7f2b351c1f5684b10ca4bd8266a
Jake Young <jake@acpdecisions.org>
2017-06-01 12:32:43 -0400

    Project setup with DubDubCore as framework

---
+++ DubDubCore/DubDubCore.h
@@ -0,0 +1,19 @@
+//
+//  DubDubCore.h
+//  DubDubCore
+//
+//  Created by Jake Young on 6/1/17.
+//  Copyright © 2017 Jake Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DubDubCore.
+FOUNDATION_EXPORT double DubDubCoreVersionNumber;
+
+//! Project version string for DubDubCore.
+FOUNDATION_EXPORT const unsigned char DubDubCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DubDubCore/PublicHeader.h>
+
+
