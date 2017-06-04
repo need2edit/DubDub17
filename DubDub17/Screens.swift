05dfa8b6249188712b875db9ed56783cbd9be577
Jake Young <need2edit@gmail.com>
2017-06-03 23:04:00 -0400

    Moves screens factory class to its own file Reorganizes project

---
+++ DubDub17/Screens.swift
@@ -0,0 +1,47 @@
+//
+//  Screens.swift
+//  DubDub17
+//
+//  Created by Jake Young on 6/3/17.
+//  Copyright © 2017 Jake Young. All rights reserved.
+//
+
+import UIKit
+
+class Screens {
+
+    func initialViewControllerForStoryboard<A: UIViewController>(_ storyboard: StoryboardIdentifier) -> A {
+        guard let vc = UIStoryboard(identifier: storyboard).instantiateInitialViewController() as? A else { fatalError() }
+        return vc
+    }
+
+    func root() -> UITabBarController {
+        let tc: UITabBarController = initialViewControllerForStoryboard(.main)
+        return tc
+    }
+
+
+    func videosRoot() -> UINavigationController {
+        let vc: UINavigationController = initialViewControllerForStoryboard(.videos)
+        vc.tabBarItem = App.Tab.videos.tabBarItem
+        return vc
+    }
+
+    func schedule() -> UIViewController {
+        let vc: UINavigationController = initialViewControllerForStoryboard(.schedule)
+        vc.tabBarItem = App.Tab.schedule.tabBarItem
+        return vc
+    }
+
+    func news() -> UIViewController {
+        let vc: UINavigationController = initialViewControllerForStoryboard(.news)
+        vc.tabBarItem = App.Tab.news.tabBarItem
+        return vc
+    }
+
+    func venue() -> UIViewController {
+        let vc: UINavigationController = initialViewControllerForStoryboard(.venue)
+        vc.tabBarItem = App.Tab.venue.tabBarItem
+        return vc
+    }
+}
