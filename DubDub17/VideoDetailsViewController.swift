c5047b3399046965995960dd7e831a07425debb6
Jake Young <need2edit@users.noreply.github.com>
2017-06-05 19:36:07 -0400

    Re-org

--- DubDub17/VideoDetailsViewController.swift
+++ DubDub17/VideoDetailsViewController.swift
@@ -8,6 +8,114 @@
 
 import UIKit
 
+// MARK: - View Model
+
+public class VideoDetailsViewModel: ViewModel {
+    
+    let video: Video
+    let actionsTintColor: UIColor
+    
+    var callback: (State) -> Void
+    
+    public struct State {
+        var isWatched: Bool = false
+        var downloadStatus: DownloadStatus = .notDownloaded
+        var isFavorite: Bool = false
+    }
+    
+    var state: State = State() {
+        didSet {
+            callback(state)
+        }
+    }
+    
+    enum Action: CustomStringConvertible {
+        
+        case watched
+        case download
+        case feedback
+        
+        var description: String {
+            switch self {
+            case .watched(let isWatched):
+                return "Mark as \(isWatched)"
+            case .download:
+                return "Download Video"
+            case .feedback:
+                return "Leave Feedback"
+            }
+        }
+    }
+    
+    var actions: [Action] = [
+        .watched,
+        .download,
+        .feedback
+    ]
+    
+    
+    
+    public init(video: Video, tintColor: UIColor, callback: @escaping (State) -> Void) {
+        self.video = video
+        self.actionsTintColor = tintColor
+        self.callback = callback
+        self.callback(state)
+    }
+    
+    public func isWatchedText() -> String {
+        return state.isWatched ? "Mark as Unwatched": "Mark as Watched"
+    }
+    
+    public func titleText() -> String {
+        return video.name
+    }
+    
+    func numberOfActions() -> Int {
+        return actions.count
+    }
+    
+    func action(at indexPath: IndexPath) -> Action {
+        return actions[indexPath.row]
+    }
+    
+    public func favoriteImage() -> UIImage? {
+        return state.isFavorite ? #imageLiteral(resourceName: "StarFilled") : #imageLiteral(resourceName: "Star")
+    }
+    
+    func cellTextForAction(_ action: Action) -> String {
+        switch action {
+        case .download:
+            return action.description
+        case .watched:
+            return isWatchedText()
+        case .feedback:
+            return action.description
+        }
+    }
+    
+    func configureCell(_ cell: UITableViewCell, at indexPath: IndexPath) {
+        let action = actions[indexPath.row]
+        cell.textLabel?.text = cellTextForAction(action)
+        cell.textLabel?.textColor = actionsTintColor
+    }
+    
+    func selectVideo(at indexPath: IndexPath, using delegate: VideoDetailsViewControllerDelegate, controller: VideoDetailsViewController) {
+        switch (indexPath.section, indexPath.row) {
+        case (0, let row) where row == 0:
+            delegate.markAsWatchedRowSelected(controller, video: video)
+        case (0, let row) where row == 1:
+            delegate.downloadVideoRowSelected(controller, video: video)
+        case (0, let row) where row == 2:
+            delegate.leaveFeedbackRowSelected(controller, video: video)
+        default:
+            return
+        }
+    }
+    
+}
+
+// MARK: - View Controller Delegation
+
 protocol VideoDetailsViewControllerDelegate {
     func shareButtonTapped(_ controller: VideoDetailsViewController, video: Video)
     func favoriteButtonTapped(_ controller: VideoDetailsViewController, video: Video)
@@ -17,9 +125,14 @@ protocol VideoDetailsViewControllerDelegate {
     func leaveFeedbackRowSelected(_ controller: VideoDetailsViewController, video: Video)
 }
 
+
+// MARK: - Layout Differences for Each Device
+
 class iPhoneVideoDetailsViewController: VideoDetailsViewController { }
 class iPadVideoDetailsViewController: VideoDetailsViewController { }
 
+
+// MARK: - Video Details Controller
 class VideoDetailsViewController: UIViewController, MVVM {
     
     var video: Video? {
